465da121a78283c8554a594ab872d08a99d90818
parvezaman <emailtoamanullah@gmail.com>
2023-09-11 20:54:18 +0600

    added meals component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () => {
+    return (
+        <section className={classes.summary}>
+            <h2>Delicious food, Delivered to you!!!</h2>
+
+            <p>Choose you favourite meal from our broad collection of available meals and enjoy a delicious lunch or dinner at  your home</p>
+
+            <p>All our meals are cooked with high-quality ingrediants, just-in-time and of course with the best of chefs</p>
+        </section>
+    )
+}
+
+export default MealsSummary
