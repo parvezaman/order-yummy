a81164bb48db409c7f31d4e77d02e73c1d915e38
parvezaman <emailtoamanullah@gmail.com>
2023-09-12 21:12:54 +0600

    add to cart using context, ref, forwardRef, reducer, useState

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartContextProvider from "./store/CartContextProvider";
 
 function App() {
   const [cartIsShown, setIsCartIsShown] = useState(false)
@@ -14,14 +15,14 @@ function App() {
     setIsCartIsShown(false)
   }
   return (
-    <>
+    <CartContextProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
 
       <main>
         <Meals />
       </main>
-    </>
+    </CartContextProvider>
   );
 }
 
