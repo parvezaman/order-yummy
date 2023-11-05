d26ada6871437a73abcc24d4fa66558350feb674
parvezaman <emailtoamanullah@gmail.com>
2023-11-05 17:33:58 +0600

    animating cart button while changing item amount

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css'
 import cartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+    const [showBump, setShowBump] = useState(false)
     const cartCtx = useContext(cartContext)
+    const { items } = cartCtx
 
-    const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    const numberOfCartItems = items.reduce((currentNumber, item) => {
         return currentNumber + item.amount
     }, 0)
+
+    const btnClasses = `${classes.button} ${showBump ? classes.bump : ""}`
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return
+        }
+
+        setShowBump(true)
+
+        const timer = setTimeout(() => {
+            setShowBump(false)
+        }, 300);
+
+        // as you have a sideeffect in the useeffect  you should have a clean up function. and by default returning a function in a useEffect will act as a cleanup function \
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [items])
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.iscon}>
                 <CartIcon />
             </span>
